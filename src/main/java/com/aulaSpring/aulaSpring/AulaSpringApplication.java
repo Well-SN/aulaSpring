8540c20cdec6defe4dec68631b6b13ad76605dbe
=Wellington Nascimento <=wellingtonwsn2015@gmail.com>
2025-03-18 16:07:22 -0300

    Inicial

---
+++ src/main/java/com/aulaSpring/aulaSpring/AulaSpringApplication.java
@@ -0,0 +1,15 @@
+package com.aulaSpring.aulaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AulaSpringApplication.class, args);
+
+	}
+
+}
